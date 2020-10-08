1d456487d3a849f048ab296626db4f7eb916631f
khamsone8585 <asoulipanya@gmail.com>
2020-10-08 16:04:25 +0700

    add_edit_banner

--- src/admin/apis/rest/controllers/slideController.ts
+++ src/admin/apis/rest/controllers/slideController.ts
@@ -1,11 +1,11 @@
 import {Request, Response} from 'express'
-import slidebanner from '@/model/slidebanner'
+import slideBanner from '@/model/slidebanner'
 
 const slideController = {
     addSlide: async(req: Request, res: Response)=>{
         const {image,url}=req.body
         try{
-            const addSlides = new slidebanner({
+            const addSlides = new slideBanner({
                 image,
                 url
             })
@@ -17,7 +17,7 @@ const slideController = {
     },
     getSlide: async(req: Request, res: Response)=>{
         try{
-            const getSlides = await slidebanner.find()
+            const getSlides = await slideBanner.find()
             res.status(200).json({getSlides})
         }catch(e){
             res.status(400).json(e)
@@ -26,7 +26,7 @@ const slideController = {
     updateSlide: async(req: Request, res: Response)=>{
         const {id,image,url}=req.body
         try{
-            const updateSlides = await slidebanner.findByIdAndUpdate(id,{
+            const updateSlides = await slideBanner.findByIdAndUpdate(id,{
                 $set:{
                     image,
                     url
@@ -40,7 +40,7 @@ const slideController = {
     deleteSlide: async(req:Request, res: Response)=>{
         const{id}=req.params
         try{
-            await slidebanner.findByIdAndDelete(id)
+            await slideBanner.findByIdAndDelete(id)
             res.status(200).json('Delete Success')
         }catch(e){
             res.status(400).json(e)
@@ -49,7 +49,7 @@ const slideController = {
     findIdSlide: async(req:Request, res: Response)=>{
         const {id}=req.params
         try{
-            const findId = await slidebanner.findById(id)
+            const findId = await slideBanner.findById(id)
             res.status(200).json({findId})
         }catch(e){
             res.status(400).json(e)
