20741ce2a55c25b2f3c0d80175d5a29e5aed0d0f
khamsone8585 <asoulipanya@gmail.com>
2020-10-29 11:23:57 +0700

    fixSort

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ import {isAuth} from './plugin/passport'
 //every push and build == true
 //offline == false
 
-const isProd = true
+const isProd = false
 if (isProd) {
     moduleAlias.addAliases({
         "@": path.join(__dirname, '/../dist')
